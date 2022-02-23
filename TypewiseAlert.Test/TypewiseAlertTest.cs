9362d9214190e333d901db63cf12c2946c058ea0
Morales Gonzalez Ricardo (CI/PFM3-LA) <rri2tl@bosch.com>
2022-02-23 15:43:03 -0600

    test functions

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -34,13 +34,8 @@ namespace TypewiseAlert.Test
 
             Assert.True(_alert.sendTo == true);
 
-        }
 
-        [Fact]
-        public void TestAlertController()
-        {
-            Alerter _alert = new Alerter();
-
+            // Controller Sender
             _alert.SetTarget(new SenderController());
 
             // Test email  NORMAL
@@ -62,11 +57,24 @@ namespace TypewiseAlert.Test
 
             Assert.True(_alert.sendTo == true);
 
-
-
         }
 
-        
+
+        [Fact]
+        public void InfersPerCoolingType()
+        {
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.PASSIVE_COOLING, -1) == TypewiseAlert.BreachType.TOO_LOW);
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 36) == TypewiseAlert.BreachType.TOO_HIGH);
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 24) == TypewiseAlert.BreachType.NORMAL);
+
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, -1) == TypewiseAlert.BreachType.TOO_LOW);
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 56) == TypewiseAlert.BreachType.TOO_HIGH);
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 24) == TypewiseAlert.BreachType.NORMAL);
+
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING,  -1) == TypewiseAlert.BreachType.TOO_LOW);
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 41) == TypewiseAlert.BreachType.TOO_HIGH);
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 24) == TypewiseAlert.BreachType.NORMAL);
+        }
 
 
         [Fact]
